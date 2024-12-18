1061f4bddec0f1272b8de8af68aa2087cb1651da
Matej Valocký <74997007+matejvalocky@users.noreply.github.com>
2024-12-18 21:53:47 +0100

    vytvorenie nového vite+react projektu, vytvorenie prvej komponenty Navigation.jsx

---
+++ WEB-ARBET-2025/src/components/Navigation.jsx
@@ -0,0 +1,19 @@
+
+
+const Navigation = () => {
+  return (
+    <nav>
+      <ul>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+      </ul>
+    </nav>
+  )
+}
+
+export default Navigation
+
+  

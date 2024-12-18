7f1cda547980f55e674e7bab084e7ccfcc69a675
Matej Valocký <74997007+matejvalocky@users.noreply.github.com>
2024-12-19 00:04:40 +0100

    definovanie premennych - farby, implementovanie do Navigation.css, pridanie NavLink do Navigation.jsx

--- WEB-ARBET-2025/src/components/Navigation.jsx
+++ WEB-ARBET-2025/src/components/Navigation.jsx
@@ -1,10 +1,12 @@
 // KOMPONENTA Navigácie:
 // components/Navigation.jsx
 import { Link } from 'react-router-dom'
+import { NavLink } from 'react-router-dom';
 
 
 // importovanie css štýlov
 import "./Navigation.css"
+
 
 
 // import obrázkov
@@ -12,15 +14,25 @@ import logo from "../assets/nav-logo.avif"
 
 const Navigation = () => {
     return (
-        <nav className='navbar'>
+        <nav className='navbar box-shadow'>
             <div className="nav-wrapper">
                 <img src={logo} alt="" />
                 <ul>
-                    <li><Link to="/">Domov</Link></li>
-                    <li><Link to="/buildings">Stavby</Link></li>
-                    <li><Link to="/projects">Projekty</Link></li>
-                    <li><Link to="/about">O nás</Link></li>
-                    <li><Link to="/contact">Kontakt</Link></li>
+                    <li>
+                        <NavLink to="/" activeClassName="active">Domov</NavLink>
+                    </li>
+                    <li>
+                        <NavLink to="/buildings" activeClassName="active">Stavby</NavLink>
+                    </li>
+                    <li>
+                        <NavLink to="/projects" activeClassName="active">Projekty</NavLink>
+                    </li>
+                    <li>
+                        <NavLink to="/about" activeClassName="active">O nás</NavLink>
+                    </li>
+                    <li>
+                        <NavLink to="/contact" activeClassName="active">Kontakt</NavLink>
+                    </li>
                 </ul>
             </div>
 

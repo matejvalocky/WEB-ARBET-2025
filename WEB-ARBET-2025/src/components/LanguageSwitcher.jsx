61fec75a717b30d35face64fac7a0c53447371f5
Matej Valocký <74997007+matejvalocky@users.noreply.github.com>
2024-12-19 02:39:46 +0100

    vyskakovací zoznam jazykov

--- WEB-ARBET-2025/src/components/LanguageSwitcher.jsx
+++ WEB-ARBET-2025/src/components/LanguageSwitcher.jsx
@@ -1,15 +1,23 @@
 import React from "react";
 import { useLanguage } from "../../src/LanguageContext";
+import "./LanguageSwitcher.css";
 
 const LanguageSwitcher = () => {
-  const { switchLanguage } = useLanguage();
+  const { switchLanguage, currentLang } = useLanguage();
 
   return (
     <div className="language-switcher">
-      <button onClick={() => switchLanguage("SK")}>SK</button>
-      <button onClick={() => switchLanguage("EN")}>EN</button>
-      <button onClick={() => switchLanguage("PL")}>PL</button>
-      <button onClick={() => switchLanguage("NL")}>NL</button>
+      <label htmlFor="language-select">Zvoľ jazyk:</label>
+      <select
+        id="language-select"
+        value={currentLang}
+        onChange={(e) => switchLanguage(e.target.value)}
+      >
+        <option value="SK">Slovenský</option>
+        <option value="EN">English</option>
+        <option value="PL">Polski</option>
+        <option value="NL">Nederlands</option>
+      </select>
     </div>
   );
 };

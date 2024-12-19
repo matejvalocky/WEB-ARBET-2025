5ed6d3b080996ddb370b2583e61a34edee6f932d
Matej Valocký <74997007+matejvalocky@users.noreply.github.com>
2024-12-20 00:17:29 +0100

    nastavenie intervalu zmeny obrázku na 6 sekúnd a zmena trvania prechodu z 0.5s na 1s

--- WEB-ARBET-2025/src/components/Slideshow.jsx
+++ WEB-ARBET-2025/src/components/Slideshow.jsx
@@ -1,15 +1,13 @@
 import React, { useState, useEffect, useRef } from "react";
 import "./Slideshow.css";
 
-
-// Import obrázku
-import image1 from "../assets/slideshow-images/arbet-slideshow1.jpg"; // Cesta k obrázku
-import image2 from "../assets/slideshow-images/arbet-slideshow2.jpeg"; // Cesta k obrázku
-import image3 from "../assets/slideshow-images/arbet-slideshow3.jpeg"; // Cesta k obrázku
-
+// Import obrázkov
+import image1 from "../assets/slideshow-images/arbet-slideshow1.jpg";
+import image2 from "../assets/slideshow-images/arbet-slideshow2.jpeg";
+import image3 from "../assets/slideshow-images/arbet-slideshow3.jpeg";
 
 const Slideshow = () => {
-    const images = [image1, image2, image3]; // Použitie obrázka v poli
+  const images = [image1, image2, image3]; // Pole obrázkov
 
   const [currentIndex, setCurrentIndex] = useState(0);
   const [isPaused, setIsPaused] = useState(false);
@@ -23,14 +21,14 @@ const Slideshow = () => {
       setCurrentIndex((prevIndex) =>
         prevIndex === images.length - 1 ? 0 : prevIndex + 1
       );
-    }, 3000);
+    }, 6000); // Nastavenie intervalu na 6 sekúnd
 
-    return () => clearInterval(interval); // Vyčisti interval
+    return () => clearInterval(interval); // Vyčisti interval pri odchode
   }, [isPaused, currentIndex, images.length]);
 
   // Funkcie na detekciu pohybu myšou/swipe
   const handleDragStart = (e) => {
-    setIsPaused(true); // Zastavenie slideshow
+    setIsPaused(true); // Zastavenie slideshow pri ťahaní
     slideshowRef.current.startX = e.touches ? e.touches[0].clientX : e.clientX;
   };
 
@@ -40,17 +38,15 @@ const Slideshow = () => {
 
     // Urč smer posunutia na základe delta
     if (delta > 50) {
-      // Posun doľava
       setCurrentIndex((prevIndex) =>
         prevIndex === 0 ? images.length - 1 : prevIndex - 1
       );
     } else if (delta < -50) {
-      // Posun doprava
       setCurrentIndex((prevIndex) =>
         prevIndex === images.length - 1 ? 0 : prevIndex + 1
       );
     }
-    setIsPaused(false); // Pokračovanie slideshow
+    setIsPaused(false); // Pokračovanie slideshow po ťahaní
   };
 
   return (
